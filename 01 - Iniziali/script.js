af7ad51154a3e16e34172da547a6b3b0a19c516c
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-07 16:22:33 +0200

    upgrade iniziali commit

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -3,13 +3,20 @@
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
+
 // Dichiara la funzione qui.
-function initialnames(array) {
-    for (let i = 0; i < array.length; i++) {
-        const element = array[i];  
+function initialnames(arraynames) {
+    for (let i = 0; i < arraynames.length; i++) {
+        const element = arraynames[i]; 
+        const chartNames = []
+        console.log(c);
+        
+        return element.charAt(0) 
     }
 }
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(initialnames(names));
 
 
 
