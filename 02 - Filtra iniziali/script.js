50b0b7921d9c6f0040b51f56b22cd201a30d56d0
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-07 17:48:13 +0200

    upgrade filtra iniziali commit

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -4,10 +4,16 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-const result = names.filter(arraystring)
-
+const chartstr = []
 function arraystring(arraychart) {
-    return arraychart >= names
+    for (let i = 0; i < arraychart.length; i++) {
+        const element = arraychart[i];
+        const A = chartstr.push(element.charAt(0))
+        if (chartstr === A) {
+            return chartstr
+        }
+    }
+    // return chartstr
 }
 
 // Invoca la funzione qui e stampa il risultato in console
